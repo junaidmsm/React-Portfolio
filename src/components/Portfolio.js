1511730268e3c0c61a85e15a8b24e6d3563b72bf
junaidmsm <mjunaid@live.co.uk>
2023-06-04 10:50:54 +0100

    images added

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,9 @@
 import React from "react";
 import textEditorImage from "../../assets/images/textEditor.png";
 import passwordGeneratorImage from "../../assets/images/passwordGenerator.png";
-import weatherDashImage from "../../assets/images/Weather_Dash.png";
-import noteTakerImage from "../../assets/images/NoteTaker.png";
-import workDayShedulerImage from "../../assets/images/workDaySheduler.png";
+import teamProfileGeneratorImage from "../../assets/images/teamProfileGenerator.png";
+import noteTakerImage from "../../assets/images/noteTaker.png";
+import workDaySchedularImage from "../../assets/images/workDaySchedular.png";
 import techBlogImage from "../../assets/images/techBlog.png";
 import bookSearchEngineImage from "../../assets/images/bookSearchEngine.png";
 
@@ -21,21 +21,19 @@ function Portfolio() {
               <a href="">
                 {" "}
                 <img
-                  src={Image}
+                  src={bookSearchEngineImage}
                   className="my-2"
                   style={{ width: "100%" }}
-                  alt=""
+                  alt="Book-Search-Engine"
                 />
               </a>
             </div>
             <div className="job-text">
               <h4></h4>
               <p>
-                A MERN application that lets the user log in and create an
-                account. The user can receive daily affirmations. The user also
-                has the ability to ask questions to receive an answer. The user
-                also can receive a lucky number if they want. The user can
-                donate money to the website if they want to.
+              The Book search engine is an application is leveraging the entire MERN stack and with a React front end, MongoDB database, and Node.js/Express.js server and API.
+              It's set up to allow users to save book searches to the back end.
+              The application is built using Google Books API search engine build with GraphQL API and Apollo Server.{" "}
               </p>
             </div>
           </div>
@@ -44,20 +42,25 @@ function Portfolio() {
         <div className="job">
           <div className="job-info">
             <div className="job-img">
-              <a href="">
+              <a href="https://github.com/junaidmsm/Tech-Blog">
                 {" "}
                 <img
-                  src={Image}
+                  src={techBlogImage}
                   className="my-2"
                   style={{ width: "100%" }}
-                  alt=""
+                  alt="Tech-Blog"
                 />
               </a>
             </div>
             <div className="job-text">
               <h4></h4>
               <p>
-                A HTMl, CSS, and Javascript application that lets the user.{" "}
+              This application is a blog site for developers! where tech fans can share their thoughts, opinions, articles and blog posts.
+              Upon coming to the site, the homepage presents with existing blog posts and a navbar to help direct you around the site.
+              If you try to navigate away from the homepage, you will be until to until you login and thus will be redirected to the login page.
+              You will notice there is also an option to signup from the login page.
+              If you signup for an account, you will be automatically logged in.
+              Regardless of if you signup or log in, you will be redirected to your dashboard, which is your landing platform for your own posts. If you click on a post in your dashboard, you have the option to update or delete the post. If you click on a post that isn't yours from the homepage, you will be able to view comments and leave one too! Utilzing mySql, Sequelize, and sessions, your login info is stored for 30 minutes while you roam around the site.{" "}
               </p>
             </div>
           </div>
@@ -66,20 +69,20 @@ function Portfolio() {
         <div className="job">
           <div className="job-info">
             <div className="job-img">
-              <a href="">
+              <a href="https://github.com/junaidmsm/password-generator">
                 {" "}
                 <img
-                  src={Image}
+                  src={passWordGeneratorImage}
                   className="my-2"
                   style={{ width: "100%" }}
-                  alt=""
+                  alt="Password-Generator"
                 />
               </a>
             </div>
             <div className="job-text">
               <h4></h4>
               <p>
-                A HTML and CSS application that lets the user.{" "}
+              Using JavaScript and CSS I want to randomly generate a password that meets certain criteria SO THAT I can create a strong password that provides greater security.{" "}
               </p>
             </div>
           </div>
@@ -88,7 +91,7 @@ function Portfolio() {
         <div className="job">
           <div className="job-info">
             <div className="job-img">
-              <a href="">
+              <a href="https://note-taker01-app.herokuapp.com/">
                 {" "}
                 <img
                   src={noteTakerImage}
@@ -111,23 +114,22 @@ function Portfolio() {
         <div className="job">
           <div className="job-info">
             <div className="job-img">
-              <a href="">
+              <a href="https://github.com/junaidmsm/OOP-Team-Profile-Generator">
                 {" "}
                 <img
-                  src={weatherDashImage}
+                  src={teamProfileGeneratorImage}
                   className="my-2"
                   style={{ width: "100%" }}
-                  alt="Weather_Dashboard"
+                  alt="Team-Profile-Generator"
                 />
               </a>
             </div>
             <div className="job-text">
-              <h4>Weather Dashboard</h4>
+              <h4>Team Profile Generator</h4>
               <p>
-                A website that allows the user see the weather outlook for
-                multiple cities. The user can enter a city and see a 5-Day
-                Forecast. The user can see the temperature, humidity levels,
-                wind speed as well as the UV index.{" "}
+              In this project a Node.js command-line application will be build that takes in information about employees on a software engineering team and generates an HTML webpage that displays summaries for each person.
+              This Challenge will combine the use of Jest for running the unit tests and Inquirer for collecting input from the user.
+              Which has been presented successfully in the application.{" "}
               </p>
             </div>
           </div>
@@ -136,20 +138,21 @@ function Portfolio() {
         <div className="job">
           <div className="job-info">
             <div className="job-img">
-              <a href="/">
+              <a href="https://text-editor0101-app.herokuapp.com/">
                 {" "}
                 <img
-                  src={Image}
+                  src={textEditorImage}
                   className="my-2"
                   style={{ width: "100%" }}
-                  alt=""
+                  alt="Text-Editor"
                 />
               </a>
             </div>
             <div className="job-text">
               <h4></h4>
               <p>
-                A website which offers users to input.{" "}
+              The application is a web text editor where the user can create notes or code snippets with or without an internet connection and where the user can reliably retrieve them for later use. The integrated service worker and Cache API's ensure that the application will remain fully functional even without and active internet connection.
+              This application allows the user to access visited pages even if the application is offline.{" "}
               </p>
             </div>
           </div>
@@ -158,22 +161,21 @@ function Portfolio() {
         <div className="job">
           <div className="job-info">
             <div className="job-img">
-              <a href="https://floating-island-.herokuapp.com/">
+              <a href="https://github.com/junaidmsm/Work-Day-Scheduler">
                 {" "}
                 <img
-                  src={Image}
+                  src={workDaySchedularImage}
                   className="my-2"
                   style={{ width: "100%" }}
-                  alt=""
+                  alt="Work-Day-Schedular"
                 />
               </a>
             </div>
             <div className="job-text">
               <h4></h4>
               <p>
-                A blog that allows the user to view logs into their account. The user can also
-                add comments to the blog once they are logged in as well as
-                deleting their comments.{" "}
+              A simple calendar application that allows a user to save events for each hour of the day.
+              This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.{" "}
               </p>
             </div>
           </div>
